612f35f83499e0f1ef5ba4c7598b1a76b5cc8a18
510kaito <510kaito2004@gmail.com>
2022-06-15 14:15:11 +0900

    6/15

---
+++ iphone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iphone
+//
+//  Created by cl_umeda_008 on 2022/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
